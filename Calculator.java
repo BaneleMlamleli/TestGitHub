965e3ec809c957bd52e91f33300f3acd78056f81
BaneleMlamleli <mlamlelibanele@yahoo.com>
2017-06-12 02:19:31 +0200

    I have added a division method in this class

--- Calculator.java
+++ Calculator.java
@@ -4,9 +4,17 @@ public class Calculator{
 	public static void getAddition(double firstNum, double secNum){
 		System.out.println(firstNum+" + "+secNum+" = "+(firstNum + secNum));	
 	}
+
+	//Division method
+	public static void getDivision(double first, double sec){
+		if(sec == 0){
+			System.out.println("Error! Cannot divide by zero");
+		}else{
+			System.out.println(first+" / "+sec+" = "+(first/sec));
+		}
+	}
 	
 	public static void main(String[]args){
 		java.util.Scanner console = new java.util.Scanner(System.in);
 		getAddition(2,5);
 	}
-}
